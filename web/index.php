d7c5fd2c62f74ff792a9b355ef61cef2a3568d65
Adán Alonso <adan.alonso.s@gmail.com>
2019-08-06 19:10:07 +0100

    Http auth

--- web/index.php
+++ web/index.php
@@ -1,8 +1,30 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+function require_auth() {
+	$AUTH_USER = getenv('AUTH_USER');
+	$AUTH_PASS = getenv('AUTH_PASS');
+	header('Cache-Control: no-cache, must-revalidate, max-age=0');
+	$has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+	$is_not_authenticated = (
+		!$has_supplied_credentials ||
+		$_SERVER['PHP_AUTH_USER'] != $AUTH_USER ||
+		$_SERVER['PHP_AUTH_PW']   != $AUTH_PASS
+	);
+	if ($is_not_authenticated) {
+		header('HTTP/1.1 401 Authorization Required');
+		header('WWW-Authenticate: Basic realm="Access denied"');
+		exit;
+	}
+}
+
+if(getenv('PRODUCTION')) {
+  // Production environment
+  require_auth();
+} else {
+  // Development environment
+  defined('YII_DEBUG') or define('YII_DEBUG', true);
+  defined('YII_ENV') or define('YII_ENV', 'dev');
+}
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
